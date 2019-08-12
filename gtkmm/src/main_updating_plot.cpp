b7895d80ffcf1be9294c78552fa8bb38695b0091
Kanishke-Mac <kanishkegb@gwu.edu>
2019-08-12 08:51:12 -0400

    add close button

--- gtkmm/src/main_updating_plot.cpp
+++ gtkmm/src/main_updating_plot.cpp
@@ -17,7 +17,6 @@
 #include <pthread.h>
 #include <unistd.h>
 
-
 #define NUM_THREADS 2
 
 struct Params
@@ -39,18 +38,38 @@ namespace fdcl
 class gui : public Gtk::Window
 {
 private:
-    Gtk::PLplot::PlotData2D plot_ax;
+    Gtk::PLplot::PlotData2D plot_ax, *plot_keep_ax;
     Gtk::PLplot::PlotData2D *plot_update;
+    Gtk::PLplot::Plot2D *plot_keep;
     Gtk::PLplot::Plot2D *plot;
     Gtk::PLplot::Canvas canvas;
     Gtk::Grid grid;
+    Gtk::Grid button_grid;
+
+    Gtk::Button close;
 public:
     gui(std::valarray<double> &x, std::valarray<double> &y):
-        plot_ax(x, y)
+        plot_ax(x, y),
+        close("Exit")
     {
-        // canvas.set_focus_on_click(false);
-        // canvas.freeze_child_notify();
         canvas.set_can_focus(false);
+        // canvas.on_motion_notify_event()
+        canvas.add_events(Gdk::POINTER_MOTION_MASK |
+             Gdk::BUTTON_PRESS_MASK |
+             Gdk::BUTTON_RELEASE_MASK);
+        // canvas.on_motion_notify_event()
+        // canvas.signal_clicked().connect(
+        // sigc::mem_fun(canvas, &gui::on_plplot_drawing_area_double_click));
+
+        std::valarray<double> xx(10), yy(10);
+        for (int i = 0; i < 10; i++)
+        {
+            xx[i] = i;
+            yy[i] = 5.0;
+        }
+        plot_keep_ax = Gtk::manage(new Gtk::PLplot::PlotData2D(xx, yy));
+        plot_keep = Gtk::manage(new Gtk::PLplot::Plot2D(*plot_keep_ax));
+        canvas.add_plot(*plot_keep);
 
         plot = Gtk::manage(new Gtk::PLplot::Plot2D(plot_ax));
         canvas.add_plot(*plot);
@@ -65,33 +84,70 @@ public:
         canvas.set_hexpand(true);
         canvas.set_vexpand(true);
 
-        grid.attach(canvas, 0, 0, 1, 1);
+
+        button_grid.set_row_spacing(5);
+        button_grid.set_column_spacing(5);
+        button_grid.set_column_homogeneous(false);
+        close.signal_clicked().connect(sigc::mem_fun(*this, &gui::on_close_button_clicked));
+        button_grid.attach(close, 1, 0, 1, 1);
+
+        grid.attach(button_grid, 0, 0, 1, 1);
+        grid.attach(canvas, 0, 1, 1, 1);
         add(grid);
         grid.show_all();
 
         //create slot for timeout signal
         int timeout_value = 50; //in ms 20 Hz
         sigc::slot<bool> my_slot = sigc::mem_fun(*this, &gui::on_timeout);
-        //connect slot to signal
         Glib::signal_timeout().connect(my_slot, timeout_value);
+
     }
+
+
+    void on_close_button_clicked(void)
+    {
+        hide();
+    }
+
 
     bool on_timeout(void)
     {
-        canvas.remove_plot(*plot);
-
         pthread_mutex_lock(&mutex);
-        plot_update = Gtk::manage(new Gtk::PLplot::PlotData2D(x, y));
-        pthread_mutex_unlock(&mutex);
+        canvas.remove_plot(*plot);
+        // Gtk::PLplot::Plot *curr_plot;
+        // auto curr_plot = canvas.get_plot(0);
+        // canvas.remove_plot(*curr_plot);
 
+        plot_update = Gtk::manage(new Gtk::PLplot::PlotData2D(x, y));
         plot = Gtk::manage(new Gtk::PLplot::Plot2D(*plot_update));
+        // std::cout << plot << std::endl;
         canvas.add_plot(*plot);
+        // canvas.remove_plot(1);
+
+        // canvas.
+        pthread_mutex_unlock(&mutex);
 
         return true;
     }
+
+    bool on_plplot_drawing_area_double_click(GdkEventButton *event) 
+    {
+        if (event->type == GDK_2BUTTON_PRESS) 
+        {
+            // plplot_drawing_area.set_region(xmin, xmax, ymin, ymax);
+        }
+        return false;
+    }
+
+
+
 };  // end of class gui
 }  // end of namespace fdcl
 
+  bool Gtk::PLplot::Canvas::on_motion_notify_event(GdkEventMotion *event)
+  {
+      return true;
+  }
 
 void thread_gui(void)
 {
@@ -116,13 +172,12 @@ void thread_gui(void)
 
 void *thread_change_data(void *thread_id)
 {
-    std::cout << "A" << std::endl;
     while (system_on)
     {
         pthread_mutex_lock(&mutex);
         y[0] = y[0] + 0.01;
         pthread_mutex_unlock(&mutex);
-        std::cout << y[0] << std::endl;
+        
         usleep(50000);  // 50 millis
     }
 
