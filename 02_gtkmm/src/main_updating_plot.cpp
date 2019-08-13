a7f6ef042d542ebd366be0ade44c608a3bda270f
Desktop <kanishkegb@gwu.edu>
2019-08-12 21:44:49 -0400

    add licence data

--- 02_gtkmm/src/main_updating_plot.cpp
+++ 02_gtkmm/src/main_updating_plot.cpp
@@ -1,19 +1,34 @@
+/* 
+ * Copyright (c) 2019 Flight Dynamics and Control Lab
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #include "gtkmm-plplot.h"
 #include <gtkmm/application.h>
 #include <glibmm/miscutils.h>
 #include <glib.h>
 #include <gtkmm.h>
 #include <gtkmm/window.h>
-// #include <gtkmm/switch.h>
-// #include <gtkmm/label.h>
-// #include <gtkmm/comboboxtext.h>
 #include <gtkmm/grid.h>
-// #include <gtkmm/spinbutton.h>
-// #include <gtkmm/colorbutton.h>
-// #include <gtkmm/pagesetup.h>
 
 #include <iostream>
-// #include <thread>
 #include <pthread.h>
 #include <unistd.h>
 
